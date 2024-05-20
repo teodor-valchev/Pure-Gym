1616ac358c254c643fb385bbe336ba66c321de4f
teodor-valchev <teo.thenx@gmail.com>
2024-05-20 19:16:15 +0300

    Created components for Header and Carousel

---
+++ src/components/Header.jsx
@@ -0,0 +1,119 @@
+function Header() {
+    return (
+        <div className="container-fluid bg-dark px-0">
+            <div className="row gx-0">
+                <div className="col-lg-3 bg-dark d-none d-lg-block">
+                    <a
+                        className="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center"
+                        href="index.html"
+                    >
+                        <h1 className="m-0 display-4 text-primary text-uppercase">
+                            Gymster
+                        </h1>
+                    </a>
+                </div>
+                <div className="col-lg-9">
+                    <div className="row gx-0 bg-secondary d-none d-lg-flex">
+                        <div className="col-lg-7 px-5 text-start">
+                            <div className="h-100 d-inline-flex align-items-center py-2 me-4">
+                                <i className="fa fa-envelope text-primary me-2" />
+                                <h6 className="mb-0">teo.thenx@gmail.com</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <nav className="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0 px-lg-5">
+                        <a
+                            className="navbar-brand d-block d-lg-none"
+                            href="index.html"
+                        >
+                            <h1 className="m-0 display-4 text-primary text-uppercase">
+                                Gymster
+                            </h1>
+                        </a>
+                        <button
+                            className="navbar-toggler"
+                            data-bs-target="#navbarCollapse"
+                            data-bs-toggle="collapse"
+                            type="button"
+                        >
+                            <span className="navbar-toggler-icon" />
+                        </button>
+                        <div
+                            className="collapse navbar-collapse justify-content-between"
+                            id="navbarCollapse"
+                        >
+                            <div className="navbar-nav mr-auto py-0">
+                                <a
+                                    className="nav-item nav-link active"
+                                    href="index.html"
+                                >
+                                    Home
+                                </a>
+                                <a
+                                    className="nav-item nav-link"
+                                    href="about.html"
+                                >
+                                    About
+                                </a>
+                                <a
+                                    className="nav-item nav-link"
+                                    href="class.html"
+                                >
+                                    Classes
+                                </a>
+                                <a
+                                    className="nav-item nav-link"
+                                    href="team.html"
+                                >
+                                    Trainers
+                                </a>
+                                <div className="nav-item dropdown">
+                                    <div className="dropdown-menu rounded-0 m-0">
+                                        <a
+                                            className="dropdown-item"
+                                            href="blog.html"
+                                        >
+                                            Blog Grid
+                                        </a>
+                                        <a
+                                            className="dropdown-item"
+                                            href="detail.html"
+                                        >
+                                            Blog Detail
+                                        </a>
+                                        <a
+                                            className="dropdown-item"
+                                            href="testimonial.html"
+                                        >
+                                            Testimonial
+                                        </a>
+                                    </div>
+                                </div>
+                                <a
+                                    className="nav-item nav-link"
+                                    href="contact.html"
+                                >
+                                    Contact
+                                </a>
+                                <a className="nav-item nav-link" href="#">
+                                    Register
+                                </a>
+                                <a className="nav-item nav-link" href="#">
+                                    Login
+                                </a>
+                            </div>
+                            <a
+                                className="btn btn-primary py-md-3 px-md-5 d-none d-lg-block"
+                                href=""
+                            >
+                                Join Us
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/HeaderCarouser.jsx
@@ -0,0 +1,108 @@
+function HeaderCoursel() {
+    return (
+        <div className="container-fluid p-0 mb-5">
+            <div
+                className="carousel slide"
+                data-bs-ride="carousel"
+                id="header-carousel"
+            >
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img
+                            alt="Image"
+                            className="w-100"
+                            src="src/public/img/carousel-1.jpg"
+                        />
+                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div
+                                className="p-3"
+                                style={{
+                                    maxWidth: "900px",
+                                }}
+                            >
+                                <h5 className="text-white text-uppercase">
+                                    Best Gym Center
+                                </h5>
+                                <h1 className="display-2 text-white text-uppercase mb-md-4">
+                                    Build Your Body Strong With Gymster
+                                </h1>
+                                <a
+                                    className="btn btn-primary py-md-3 px-md-5 me-3"
+                                    href=""
+                                >
+                                    Join Us
+                                </a>
+                                <a
+                                    className="btn btn-light py-md-3 px-md-5"
+                                    href=""
+                                >
+                                    Contact Us
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img
+                            alt="Image"
+                            className="w-100"
+                            src="src/public/img/carousel-2.jpg"
+                        />
+                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div
+                                className="p-3"
+                                style={{
+                                    maxWidth: "900px",
+                                }}
+                            >
+                                <h5 className="text-white text-uppercase">
+                                    Best Gym Center
+                                </h5>
+                                <h1 className="display-2 text-white text-uppercase mb-md-4">
+                                    Grow Your Strength With Our Trainers
+                                </h1>
+                                <a
+                                    className="btn btn-primary py-md-3 px-md-5 me-3"
+                                    href=""
+                                >
+                                    Join Us
+                                </a>
+                                <a
+                                    className="btn btn-light py-md-3 px-md-5"
+                                    href=""
+                                >
+                                    Contact Us
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    data-bs-slide="prev"
+                    data-bs-target="#header-carousel"
+                    type="button"
+                >
+                    <span
+                        aria-hidden="true"
+                        className="carousel-control-prev-icon"
+                    />
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    data-bs-slide="next"
+                    data-bs-target="#header-carousel"
+                    type="button"
+                >
+                    <span
+                        aria-hidden="true"
+                        className="carousel-control-next-icon"
+                    />
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+} 
+
+export default HeaderCoursel
