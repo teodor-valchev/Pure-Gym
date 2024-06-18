c01e665e79abf5889490a0f50166ff3e9ace53d0
teodor-valchev <teo.thenx@gmail.com>
2024-06-18 10:34:59 +0300

    added headline

--- client/src/components/classes/class-detail/ClassDetails.jsx
+++ client/src/components/classes/class-detail/ClassDetails.jsx
@@ -30,7 +30,7 @@ const ClassDetails = () => {
                 <div className="row py-5">
                     <div className="col-12 text-center">
                         <h1 className="display-2 text-uppercase text-white mb-md-4">
-                            Blog Detail
+                            Class Detail
                         </h1>
                         <Link
                             to={Path.Home}
