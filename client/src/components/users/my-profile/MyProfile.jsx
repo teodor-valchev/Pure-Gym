1792763a7db5275a1d00a77b4b077dd5718e0322
teodor-valchev <teo.thenx@gmail.com>
2024-07-10 12:19:40 +0300

    Removed unused import

--- client/src/components/users/my-profile/MyProfile.jsx
+++ client/src/components/users/my-profile/MyProfile.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 
 import * as authService from "../../../services/authService.js";
 import "./MyProfile.css";
